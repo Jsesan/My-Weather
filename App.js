e7d54851374aa667ccd47af1a229d841de119468
Jsesan <jsesan00@gmail.com>
2022-04-13 13:34:28 -0300

    [main] add styles and text

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Platform, TextInput, KeyboardAvoidingView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <KeyboardAvoidingView style={styles.container} behavior={'padding'}>
+      <Text style={[styles.textStyle, styles.largeText]}>Paso del Rey</Text>
+      <Text style={[styles.textStyle, styles.smallText]}>Light Cloud</Text>
+      <Text style={[styles.textStyle, styles.largeText]}>24°</Text>
+      <TextInput autoCorrect={false} placeholder={'Search any city'} placeholderTextColor={'white'} style={styles.textInput} clearButtonMode={'always'}/>
       <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -17,4 +20,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    color:'white',
+    fontFamily:
+      Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto'
+  },
+  largeText : {
+    fontSize: 44,
+  },
+  smallText:{
+    fontSize: 18
+  },
+  textInput:{
+    backgroundColor: '#666',
+    color:'black',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center'
+  }
+
 });
